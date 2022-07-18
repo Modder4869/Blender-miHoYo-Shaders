cd3f8890ff0846f1542010a12f7339562c668571
Modder4869 <28317935+Modder4869@users.noreply.github.com>
2022-07-18 16:15:57 +0200

    use material name instead of index

--- scripts/genshin-import-linear.py
+++ scripts/genshin-import-linear.py
@@ -48,41 +48,41 @@ class GI_OT_GenshinImportTextures(Operator, ImportHelper):
 				# Implement the texture in the correct node
 				if "Hair_Diffuse" in file :
 					bpy.context.view_layer.objects.active = body_var
-					bpy.context.object.material_slots[0].material.node_tree.nodes['Hair_Diffuse_UV0'].image = img
-					bpy.context.object.material_slots[0].material.node_tree.nodes['Hair_Diffuse_UV1'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Hair').material.node_tree.nodes['Hair_Diffuse_UV0'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Hair').material.node_tree.nodes['Hair_Diffuse_UV1'].image = img
 				elif "Hair_Lightmap" in file :
 					bpy.context.view_layer.objects.active = body_var
 					img.colorspace_settings.name='Non-Color'
-					bpy.context.object.material_slots[0].material.node_tree.nodes['Hair_Lightmap_UV0'].image = img
-					bpy.context.object.material_slots[0].material.node_tree.nodes['Hair_Lightmap_UV1'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Hair').material.node_tree.nodes['Hair_Lightmap_UV0'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Hair').material.node_tree.nodes['Hair_Lightmap_UV1'].image = img
 				elif "Hair_Normalmap" in file :
 					bpy.context.view_layer.objects.active = body_var
 					img.colorspace_settings.name='Non-Color'
-					bpy.context.object.material_slots[0].material.node_tree.nodes['Hair_Normalmap_UV0'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Hair').material.node_tree.nodes['Hair_Normalmap_UV0'].image = img
 				elif "Hair_Shadow_Ramp" in file :
 					bpy.data.node_groups['Hair Shadow Ramp'].nodes['Hair_Shadow_Ramp'].image = img
 				elif "Body_Diffuse" in file :
 					bpy.context.view_layer.objects.active = body_var
-					bpy.context.object.material_slots[1].material.node_tree.nodes['Body_Diffuse_UV0'].image = img
-					bpy.context.object.material_slots[1].material.node_tree.nodes['Body_Diffuse_UV1'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Body').material.node_tree.nodes['Body_Diffuse_UV0'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Body').material.node_tree.nodes['Body_Diffuse_UV1'].image = img
 				elif "Body_Lightmap" in file :
 					bpy.context.view_layer.objects.active = body_var
 					img.colorspace_settings.name='Non-Color'
-					bpy.context.object.material_slots[1].material.node_tree.nodes['Body_Lightmap_UV0'].image = img
-					bpy.context.object.material_slots[1].material.node_tree.nodes['Body_Lightmap_UV1'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Body').material.node_tree.nodes['Body_Lightmap_UV0'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Body').material.node_tree.nodes['Body_Lightmap_UV1'].image = img
 				elif "Body_Normalmap" in file :
 					bpy.context.view_layer.objects.active = body_var
 					img.colorspace_settings.name='Non-Color'
-					bpy.context.object.material_slots[1].material.node_tree.nodes['Body_Normalmap_UV0'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Body').material.node_tree.nodes['Body_Normalmap_UV0'].image = img
 				elif "Body_Shadow_Ramp" in file :
 					bpy.data.node_groups['Body Shadow Ramp'].nodes['Body_Shadow_Ramp'].image = img
 				elif "Face_Diffuse" in file :
 					bpy.context.view_layer.objects.active = body_var
-					bpy.context.object.material_slots[2].material.node_tree.nodes['Face_Diffuse'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Face').material.node_tree.nodes['Face_Diffuse'].image = img
 				elif "Face_Shadow" in file :
 					bpy.context.view_layer.objects.active = body_var
 					img.colorspace_settings.name='Non-Color'
-					bpy.context.object.material_slots[2].material.node_tree.nodes['Face_Shadow'].image = img
+					bpy.context.object.material_slots.get('miHoYo - Genshin Face').material.node_tree.nodes['Face_Shadow'].image = img
 				elif "FaceLightmap" in file :
 					bpy.context.view_layer.objects.active = body_var
 					img.colorspace_settings.name='Non-Color'
